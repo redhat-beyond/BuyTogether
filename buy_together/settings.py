46d845bdbb1eb145a60ba922d2f53f26509317b3
Karam Gbaren <karam99.k@hotmail.com>
2022-11-10 15:36:34 +0200

    Webpage is up

--- buy_together/settings.py
+++ buy_together/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'buy_together_app.apps.BuytogetherappConfig',
 ]
 
 MIDDLEWARE = [
