d0c4d0b45a1254560379869e4de1b627d1266ffe
RawanAbuleil <rawanwaelab@icloud.com>
2022-11-20 20:58:44 +0200

    Adding Cart Model adding 0001_initial.py uing migrations

--- buy_together_app/admin.py
+++ buy_together_app/admin.py
@@ -1,3 +1,4 @@
-# from django.contrib import admin
+from django.contrib import admin
+from buy_together_app.models import Cart
 
-# Register your models here.
+admin.site.register(Cart)
--- buy_together_app/models.py
+++ buy_together_app/models.py
@@ -1,3 +1,5 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Cart (models.Model):
+    cart_id = models.CharField(max_length=30, primary_key=True, unique=True)
